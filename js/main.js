596377d4883646345fa7f734ab91abdb8cea30eb
GraysonR <gricket@purdue.edu>
2016-08-26 21:31:55 -0400

    Main.js created and JS dimple work began.

---
+++ js/main.js
@@ -0,0 +1,16 @@
+var svg = dimple.newSvg("#chartContainer", 590, 400);
+d3.json("/data/scorecard_data.json", function (data) {
+  data = dimple.filterData(data, "School", ["avg"]);
+
+  var myChart = new dimple.chart(svg, data);
+  myChart.setBounds(60, 30, 505, 305);
+
+  var x = myChart.addCategoryAxis("x", "Year");
+  x.addOrderRule("Year");
+
+  myChart.addMeasureAxis("y", "debt_mdn");
+
+  var s = myChart.addSeries(null, dimple.plot.line);
+
+  myChart.draw();
+});
